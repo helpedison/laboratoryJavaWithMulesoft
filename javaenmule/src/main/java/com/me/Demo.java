bd42a65b2abcc6424a7acd973fc220e31f8c1102
Edison Jahir Vargas <helpedison@gmail.com>
2020-07-31 10:52:20 -0500

    se agrega el proyecto javaenmule

---
+++ javaenmule/src/main/java/com/me/Demo.java
@@ -0,0 +1,14 @@
+package com.me;
+
+public class Demo {
+
+	public static int add(int num1, int num2 ) {
+		int result = num1+ num2;
+		
+		return  result;
+		
+	}
+	
+	
+
+}
---
+++ javaenmule/src/main/java/com/me/TaxCalculator.java
@@ -0,0 +1,18 @@
+package com.me;
+
+public class TaxCalculator {
+	
+	   private Double percentBaseTax = 7.0;
+	
+	   public Double calculateTax(Double price, Integer percentAdditionalTax) {
+	        return price * (this.percentBaseTax + percentAdditionalTax) / 100;
+	    }
+
+	    public Boolean isTaxFree(Double price) {
+	        if (price < 10) {
+	            return true;
+	        }
+	        return false;
+	    }
+	
+}
